ffdd6cd1ceb1324479bbb76a672ea9055441101f
Whittney Davis <wcdavis89@yahoo.com>
2019-11-30 20:17:45 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -22,28 +22,3 @@ class Post
 
 end
 
-
-class Author 
-  attr_accessor :name, :post
-  
-  @@post_count=0
-  
- def initialize(name)
-    @name = name
-    @posts = []
-  end
-
-  def add_post(post)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-  def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-end
