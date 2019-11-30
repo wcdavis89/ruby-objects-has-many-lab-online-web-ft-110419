cb03269f6c599db4bf6d0eeb1b8d97cadf994e6d
Whittney Davis <wcdavis89@yahoo.com>
2019-11-30 20:20:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,32 +21,3 @@ class Song
   end
 
 end
-
-
-class Artist 
-  attr_accessor :name, :song
-
-  def initialize(name)
-    @name=name
-    @songs=[]
-  end
-  
-  def add_song(song)
-    song.artist=self
-    @songs << song
-  end
-  
-  def add_song_by_name(name)
-    song=Song.new(name)
-    add_song(song)
-  end
-  
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
-  
-  def self.song_count
-    @songs.size +=1
-  end
-    
-end
