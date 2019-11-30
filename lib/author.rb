25a4f317c8185816730dc15d975fc1902bbe24dd
Whittney Davis <wcdavis89@yahoo.com>
2019-11-30 20:19:36 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,25 +1,27 @@
-
 class Author 
   attr_accessor :name, :post
-  
-  @@post_count=0
-  
- def initialize(name)
-    @name = name
-    @posts = []
+@@post_count=0
+  def initialize(name)
+    @name=name
+    @posts=[]
   end
-
+  
   def add_post(post)
+    post.author=self
     @posts << post
-    post.author = self
-    @@post_count += 1
   end
-
+  
   def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post
+    post=Song.new(title)
     post.author = self
-    @@post_count += 1
+    @posts << post
+  end
+  
+  def posts
+    Post.all.select {|post| post.author == self}
   end
 
+  def self.post_count
+    @posts.length
+  end
 end
